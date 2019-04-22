a703edd2ab472cd5b9fec8852122a78aaf95c38d
Grociu <jan.grottel@gmail.com>
2019-04-22 11:51:56 +0200

    Code cleanup, PEP8 check

--- Python3/exercism/armstrong-numbers/armstrong_numbers.py
+++ Python3/exercism/armstrong-numbers/armstrong_numbers.py
@@ -1,14 +1,12 @@
 def is_armstrong(number):
-    """
-    This function checks if the number is an Armstrong number -
-    (If the sum of it's digits to the power of it's length is equal
-    to the number itself).
+    """ This function checks if a number is an Armstrong number.
+    
+    An Armstrong number is an integer for which the sum of it's digits to the 
+    power of it's length is equal to the number itself.
     """
     armstrong = 0
-    # lists the digits of the number
     digits = [int(x) for x in str(number)]
-    # increments the armstrong value by each power
-    for i in range(0,len(digits)):
+    for i in range(len(digits)):
         armstrong += digits[i] ** len(digits)
-    # checks if the armstrong value is equal to the original
+
     return (armstrong == number)
