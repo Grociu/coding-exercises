7a4136ba72d06f757cbf5ce0c7ea6df254487e0c
Grociu <jan.grottel@gmail.com>
2019-04-22 17:23:41 +0200

    PEP8

--- Python3/exercism/pangram/pangram.py
+++ Python3/exercism/pangram/pangram.py
@@ -1,13 +1,14 @@
 """
-This program will check if a given string contains 
-all the letters of the English alphabeth.
+This program will check if a given string contains all the letters of the
+English alphabeth.
 https://en.wikipedia.org/wiki/Pangram
 """
 # define alphabeth
 alphabeth = "abcdefghijklmnopqrstuvwxyz"
 
+
 def is_pangram(sentence):
-    """This function checks if a given sentence is a pangram"""
+    """ This function checks if a given sentence is a pangram"""
     # make sentence lowercase
     sentence = sentence.lower()
     # iterate for letters in alphabeth, if letter not found, return false
