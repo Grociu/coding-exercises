5f00c66298c32ea5a3dd4e5b78744cb3db357b88
Grociu <jan.grottel@gmail.com>
2019-04-22 12:22:31 +0200

    Code cleanup, PEP8

--- Python3/exercism/dnd-character/dnd_character.py
+++ Python3/exercism/dnd-character/dnd_character.py
@@ -1,16 +1,20 @@
 """
-This contains the Character class, that generates 
-a random DnD character to given specification
+This contains the Character class, that generates a random DnD character to
+given specification.
 """
-
 from random import randint
 
+
 def modifier(attribute):
-    """This generates a modifier for an attribute."""
-    """I think the test wants this to be outside the Character class"""
+    """ Given the value of an attribute, returns modifier.
+
+    I think the test wants this function to be outside the Character class.
+    """
     return (attribute - 10) // 2
 
-class Character:
+
+class Character(object):
+    """ A character with it's attributes and hit points."""
     def __init__(self):
         self.strength = self.ability()
         self.dexterity = self.ability()
@@ -21,7 +25,7 @@ class Character:
         self.hitpoints = 10 + modifier(self.constitution)
     
     def ability(self):
-        """This function generates a random ability score"""
-        rolls = [randint(1,6) for x in range(0,4)]
+        """ This function generates a random ability score."""
+        rolls = [randint(1,6) for x in range(4)]
         rolls.remove(min(rolls))
         return sum(rolls)
