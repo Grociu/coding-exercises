6aa7bea7e10dd4e02a9856583a7b9ad034562150
Grociu <jan.grottel@gmail.com>
2019-04-30 15:01:42 +0200

    2nd pass of the exercise, better structure

--- Python3/exercism/matrix/matrix.py
+++ Python3/exercism/matrix/matrix.py
@@ -1,15 +1,23 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix_string = matrix_string
-        self.matrix_list = self.matrix_string.splitlines()
-   
+        self.matrix_as_lists = self.parse_matrix()
+
+    def parse_matrix(self):
+        """Takes the origial string and turns it into a list of lists."""
+        stage1 = self.matrix_string.splitlines()
+        stage2 = [row_string.split() for row_string in stage1]
+        for i in range(len(stage2)):
+            for j in range(len(stage2[i])):
+                stage2[i][j] = int(stage2[i][j])
+        return stage2
+
     def row(self, index):
-        """ Divides the list of splitlines strings into
-        a list of individual numbers.
-        Then gets a list of numbers at i=index-1.
-        """
-        return [int(i) for i in self.matrix_list[index-1].split()]
+        """For a given number, returns that row of the matrix a list."""
+        return self.matrix_as_lists[index-1]
 
     def column(self, index):
-        """ From each list of rows, get a new list of item at index-1."""
-        return [self.row(i+1)[index-1] for i in range(len(self.matrix_list))]
+        """From a given number, returns that column of the matrix as a list."""
+        return [
+            self.matrix_as_lists[i][index-1]
+            for i in range(len(self.matrix_as_lists))]
